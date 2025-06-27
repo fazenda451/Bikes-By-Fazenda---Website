d580436d942aac3a156a7a79dd18e7c5ff951cc2
Rodrigo Fazenda <rodrigo.m.fazenda@gmail.com>
2025-06-27 07:13:57 +0100

    LIMPEZA CRÍTICA: remover .env e arquivos .bat sensíveis

--- !!!Start-server-on.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Iniciando Laravel Server...
-cd /d "%~dp0"
-php artisan serve --host=0.0.0.0 --port=8000
-pause
--- !!!start-server.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d C:\Users\RodrigoFazenda\Documents\MyProjects\Bikes-By-Fazenda---Website
-php artisan serve
-pause
